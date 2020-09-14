8cc2bba85919d14ed6c97c8eecd317925aa9f448
Conrad Wright <easp.conrad@gmail.com>
2020-09-14 07:28:30 -0600

    Here is a solution using a for loop and Promises!
    You can read the comments I left in the code but everything seems
    to be working! Let's chat about it later if you want.

--- bhParse.js
+++ bhParse.js
@@ -1,71 +1,39 @@
-// const puppeteer = require('puppeteer');
-import puppeteer from 'puppeteer';
-// const nameList = [
-// 	'Martin Luther King Jr.',
-// 	'Harriet Tubman',
-// 	'Rosa Parks',
-// 	'Muhammad Ali',
-// 	'Jackie Robinson',
-// 	'Langston Hughes',
-// 	'Maya Angelou',
-// 	'George Washington Carver',
-// 	'Barack Obama',
-// 	'Madam C.J. Walker',
-// 	'Mae C. Jemison',
-// 	'Daniel Hale Williams',
-// 	'Garret Morgan',
-// 	'Oprah Winfrey',
-// 	'Carter G. Woodson',
-// ];
+const puppeteer = require('puppeteer')
 
-export const nameURL = (name) => {
-			const endpoint = 'https://www.biography.com/activist/';
-			let nameAlt = name.toLowerCase().replace(/\s/g, '-');
-			if (nameAlt.includes('.')) {
-				return endpoint + nameAlt.replace(/\./g, '');
-			} else {
-				return endpoint + nameAlt;
-			}
-		};
- 
-    
+const bhParse = async function (linksArray) {
+  // receives array of site urls
+  try {
+    const dataArray = [] // store array of objects of site data
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
 
-export const bhParse = async function (nameURL){
-    try {
-			/* Initiate the Puppeteer browser */
-			const browser = await puppeteer.launch();
-			const page = await browser.newPage();
-            
-			// Removes navigation Timeout
-			await page.setDefaultNavigationTimeout(0);
-			/* Go to the page and wait for it to load */
-			await page.goto(nameURL, { waitUntil: 'networkidle0' });
+    // loop over received array of site urls
+    for (let i = 0; i < linksArray.length; i++) {
+      let link = linksArray[i]
+      await page.goto(link, { waitUntil: 'networkidle0', timeout: 0 })
 
-			/* Run javascript inside of the page */
-			let data = await page.evaluate(() => {
-				let dates = document.querySelector(
-					'.m-detail-header--person-occupations'
-				).innerHTML;
-				let merit = document.querySelector('.m-person--abstract').innerHTML;
-				let image = document.querySelector('.m-person--image img').src;
-				/* Returning an object filled with the scraped data */
-				return {
-					dates,
-					merit,
-					image,
-				};
-			});
+      let data = await page.evaluate(() => {
+        const dates = document.querySelector(
+          '.m-detail-header--person-occupations'
+        ).innerHTML
+        const merit = document.querySelector('.m-person--abstract').innerHTML
+        const image = document.querySelector('.m-person--image img').src
 
-			/* Outputting what we scraped */
-			console.log(data);
+        return {
+          dates,
+          merit,
+          image,
+        }
+      })
 
-			await browser.close();
-		} catch (error) {
-        console.error(error);
+      dataArray.push(data)
     }
-};
 
-// const res = nameURLs.map((nameURL) => bhParse(nameURL));
-// console.log(res);
+    await browser.close()
+    return dataArray // return completed array of objects of site data
+  } catch (error) {
+    console.log('ERROR: ', error)
+  }
+}
 
-// module.exports = bhParse;
+module.exports = bhParse
--- index.js
+++ index.js
@@ -1,38 +1,43 @@
-// const puppeteer = require('puppeteer');
-import puppeteer from 'puppeteer';
-import { bhParse, nameURL } from './bhParse.js';
+const puppeteer = require('puppeteer')
+const bhParse = require('./bhParse')
 
-const bioURL = 'https://www.biography.com/tag/black-history#:~:text=While%20Black%20History%20Month%20is,profound%20impact%20in%20history%3A%20self%2D';
-// const bhParse = require('./bhParse');
+const bioURL =
+  'https://www.biography.com/tag/black-history#:~:text=While%20Black%20History%20Month%20is,profound%20impact%20in%20history%3A%20self%2D'
 
-(async () => {
-	try {
-		/* Initiate the Puppeteer browser */
-		const browser = await puppeteer.launch();
-		const page = await browser.newPage();
-		
-		// Removes navigation Timeout
-		await page.setDefaultNavigationTimeout(0);
-		/* Go to the IMDB Movie page and wait for it to load */
-		await page.goto(bioURL, { waitUntil: 'networkidle0' });
+const urls = async () => {
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
 
-		/* Run javascript inside of the page */
-		let data = await page.evaluate(() => {
-			let link = [...document.querySelectorAll('.m-hub-header--description p a')]
-			let names = link.map(item => item.innerHTML)
-			/* Returning an object filled with the scraped data */
-			return names
-		});
-		let bhURLs = await data.map( name => nameURL(name));
-	
-		/* Outputting what we scraped */
-		let bhData = bhURLs.map((bhURL) => bhParse(bhURL));
-		console.log(bhData)
-		await browser.close();
-		
-	} catch (error) {
-		console.log(error)
-	}
-})();
+    await page.goto(bioURL, { waitUntil: 'networkidle0', timeout: 0 })
+    const personalSiteLinks = await page.evaluate(() => {
+      let link = [
+        ...document.querySelectorAll('.m-hub-header--description p a'),
+      ]
+      return link.map((item) => item.href)
+    })
 
-export {puppeteer}
+    await browser.close()
+    return personalSiteLinks
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const dataCrawler = async () => {
+  try {
+    const targetUrls = Promise.resolve(urls())
+    targetUrls.then((urls) => {
+      // passing the entire array of urls to bhParse instead of
+      // running a function on each individual array item
+      Promise.resolve(bhParse(urls)).then((response) =>
+        // do whatever you need with response
+        console.log('response: ', response)
+      )
+    })
+  } catch (error) {}
+}
+
+dataCrawler()
+
+module.export = { puppeteer }
